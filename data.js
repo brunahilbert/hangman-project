35f7595c72c0184416af8b7dfefbebca16ba52f9
brunahilbert <bruna.hilbertt@gmail.com>
2023-08-11 17:03:09 +0200

    add new words to the list of words to be guessed

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
-var words = ['apple', 'banana', 'computer', 'elephant', 'guitar', 'happy', 'internet', 'jump', 'kangaroo', 'lemon', 'monkey', 'ninja', 'orange', 'pizza', 'queen', 'robot', 'soccer', 'tiger', 'unicorn', 'volleyball'];
-var tips = ['Fruit', 'Fruit', 'Play games', 'Animal', 'Instrument', 'Emotinal state', 'Computer', 'Movement', 'Animal', 'Fruit', 'Animal', 'Martial arts', 'Color', 'Food', 'Royals', 'Artificial intelligence', 'Game', 'Animal', 'Mystical creature', 'Game'];
+var words = ['apple', 'banana', 'computer', 'elephant', 'guitar', 'happy', 'internet', 'jump', 'kangaroo', 'lemon', 'monkey', 'ninja', 'orange', 'pizza', 'queen', 'robot', 'soccer', 'tiger', 'unicorn', 'volleyball', 'developer', 'javascript', 'challenge', 'debugging', 'succulent', 'technology', 'creativity', 'accessibility', 'animation', 'grid', 'responsive'];
+var tips = ['Fruit', 'Fruit', 'Play games', 'Animal', 'Instrument', 'Emotinal state', 'Connectivity', 'Movement', 'Animal', 'Fruit', 'Animal', 'Martial arts', 'Color', 'Food', 'Royals', 'Artificial intelligence', 'Game', 'Animal', 'Mystical creature', 'Game', 'Software', 'Code', 'Difficulty', 'Fixing', 'Plant', 'Innovation', 'Imagination', 'Inclusive design', 'Motion effects', 'Layout system', 'Adaptable'];
 const state = [
   '',
   `
